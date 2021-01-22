6fa0f94f7893ecd90c0db76280628037969d63c4
Dinesh I <dineshiyyandhurai@gmail.com>
2021-01-22 13:24:17 +0530

    Initial Commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "97667eff0a54522fc5df";
+/******/ 	var hotCurrentHash = "8619ea21288c4b059c40";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
